2d6400625f2c4301516e0107000f81cbfb37b68c
CPSC 473 <cpsc473@cpsc473.(none)>
2012-04-20 01:40:42 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1e2201f853e900d30e888fc3456c2b9d77e8e9741d3e14758af55162d10ecd3317334f5711e94c36ed7f70ac7379caa43d3cc3e6284f6472fbcc10df6079d20e'
